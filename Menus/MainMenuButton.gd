5bd114f7c9528d238753c4211dac45f6de2ca244
Constantine Khamis <costikhamis@gmail.com>
2025-11-11 20:14:59 -0800

    Increased system performance and stability. Also main menu systems

--- Menus/MainMenuButton.gd
+++ Menus/MainMenuButton.gd
@@ -1,9 +1,20 @@
 class_name MenuBlock
 extends RigidBody3D
 
+@export var button_action:modes = modes.START
+@onready var animation_player:=$"../AnimationPlayer"
+@onready var animation_tree:=$"../AnimationTree"
+@onready var label:=$"Label3D"
+@export var button_name:String = "Block"
 
+enum modes {
+	START,
+	OPTIONS,
+	QUIT
+}
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	label.text = button_name
 	pass # Replace with function body.
 
 
@@ -12,5 +23,12 @@ func _process(delta: float) -> void:
 	pass
 	
 func pressed():
-	get_tree().change_scene_to_file("res://test_world.tscn")
-	
+	match button_action:
+		modes.START:
+			get_tree().change_scene_to_file("res://test_world.tscn")
+		modes.QUIT:
+			get_tree().quit(0)
+		modes.OPTIONS:
+			# animation_tree.set("parameters/conditions/more_options_clicked", true)
+			animation_player.play("more_options")
+			print("rat")
